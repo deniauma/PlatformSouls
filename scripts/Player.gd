c2dbf32728b57bd94772077385140c566efc6ccd
deniauma <mathieu.deniaud@gmail.com>
2020-04-22 08:09:42 +0200

    Update player script

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -10,3 +10,13 @@ func get_direction() -> Vector2:
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		-1.0 if Input.is_action_just_pressed("ui_up") and is_on_floor() else 1.0
 	)
+
+func calculate_move_velocity(
+		linear_velocity: Vector2, 
+		direction: Vector2, 
+		speed: Vector2
+	) -> Vector2:
+	var new_velocity: = linear_velocity
+	new_velocity.x = speed.x * direction.x
+	new_velocity.y += gravity
+	return speed * velocity
